3b8c9fc565817c2f3dab36d835a4abf26c5b0c2d
ambarish-prakash <t_praka@SINSGH93407YY.ads.autodesk.com>
2013-10-03 13:04:50 +0800

    random save 1

---
+++ src/DynamoCore/Nodes/CodeBlockNode.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProtoCore.AST;
+using ProtoCore.AST.AssociativeAST;
+using Dynamo.Models;
+
+namespace Dynamo.Nodes
+{
+    [NodeName("Code Block")]
+    [NodeCategory(BuiltinNodeCategories.CORE_LISTS)]
+    [NodeDescription("Allows for code to be wriiten")] //<--Change the descp :|
+    public class DynamoCodeBlockNode : NodeModel
+    {
+
+    }
+
+
+    //NOT TESTED
+    public class Statement
+    {
+        public enum State 
+        { 
+            Normal, 
+            Warning, 
+            Error 
+        }
+        public enum StatementType 
+        { 
+            Expression, 
+            Literal, 
+            Collection, 
+            AssignmentVar, 
+            FuncDeclaration 
+        }
+
+        #region Public Methods
+        public static Statement CreateInstance(Node astNode)
+        {
+            if (astNode == null)
+                throw new ArgumentNullException();
+
+            return new Statement(astNode);
+        }
+
+        //NOT TESTED WITH THIS COMMIT --- WILL BE CHNAGED TO USE EXISTING APIs
+        public static List<Variable> GetReferencedVariables(Node astNode)
+        {
+            //DFS Search to find all identifier nodes
+            if (astNode == null)
+                return new List<Variable>();
+            List<Variable> resultList = new List<Variable>();
+            if (astNode is BinaryExpressionNode) // As of now only implemented for BEN, Will now look into other APIs and change it to universal
+            {
+                List<Variable> resultLeft;
+                resultLeft = GetReferencedVariables((astNode as BinaryExpressionNode).LeftNode);
+                resultList = GetReferencedVariables((astNode as BinaryExpressionNode).RightNode);
+                foreach (Variable var in resultLeft)
+                {
+                    if (resultList.Where(x => x.Name.Equals(var.Name)).Count() == 0)
+                    {
+                        resultList.Add(var);
+                    }
+                }
+            }
+            if (astNode is IdentifierNode)
+            {
+                Variable resultVariable = new Variable(astNode as IdentifierNode);
+                resultList.Add(resultVariable);
+            }
+            return resultList;
+        }
+        #endregion
+
+        #region Properties
+        public int StartLine { get; private set; }
+        public int EndLine { get; private set; }
+        
+        public Variable AssignedVariable { get; private set; }
+        public List<Variable> ReferencedVariables { get; private set; }
+        
+        public State CurrentState { get; private set; }
+        public StatementType CurrentType { get; private set; }
+        
+        public List<Statement> SubStatements { get; private set; }
+        #endregion
+
+        #region Private Methods
+        private Statement(Node astNode)
+        {
+            StartLine = astNode.line;
+            EndLine = astNode.endLine;
+
+            if (astNode is ProtoCore.AST.AssociativeAST.BinaryExpressionNode)
+            {
+                BinaryExpressionNode binExprNode = astNode as BinaryExpressionNode;
+                if (binExprNode.Optr == ProtoCore.DSASM.Operator.assign)
+                {
+                    if (binExprNode.LeftNode != null && binExprNode.LeftNode is IdentifierNode)
+                    {
+                        if (!binExprNode.LeftNode.Name.Equals("return"))
+                            AssignedVariable = new Variable(binExprNode.LeftNode as IdentifierNode);
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    if (binExprNode.RightNode == null)
+                    {
+                        throw new ArgumentNullException();
+                    }
+                    else
+                    {
+                        CurrentType = StatementType.Expression;
+                        ReferencedVariables = GetReferencedVariables(binExprNode.RightNode);
+                    }
+
+                }
+            }
+            else if (astNode is IdentifierNode)
+            {
+                CurrentType = StatementType.AssignmentVar;
+            }
+            else if (astNode is ExprListNode)
+            {
+                CurrentType = StatementType.Collection;
+            }
+            else if (astNode is FunctionDefinitionNode)
+            {
+                FunctionDefinitionNode funcNode = astNode as FunctionDefinitionNode;
+                CodeBlockNode funcBody = funcNode.FunctionBody;
+                foreach (Node node in funcBody.Body)
+                {
+                    Statement tempStatement = new Statement(node);
+                    SubStatements.Add(tempStatement);
+                }
+            }
+        }
+        #endregion
+    }
+
+    public class Variable
+    {
+        public int Row {get; private set;}
+        public int StartColumn { get; private set; }
+        public int EndColumn { get; private set; }
+        public string Name { get; private set; }
+
+        public Variable(IdentifierNode identNode)
+        {
+            if (identNode == null)
+                throw new ArgumentNullException();
+            Name = identNode.Value;
+            Row = identNode.line;
+            StartColumn = identNode.col;
+            EndColumn = identNode.endCol;
+        }
+    }
+}
---
+++ src/DynamoElementsTests/CodeBlockNodeTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NUnit.Framework;
+using ProtoCore;
+using ProtoCore.AST.AssociativeAST;
+using Dynamo;
+using Dynamo.Nodes;
+
+
+namespace Dynamo.Tests
+{
+    class CodeBlockNodeTests : DynamoUnitTest
+    {
+        [Test]
+        public void TestVariableClass()
+        {
+            string code = @"def foo(){return=10;}";
+            CodeBlockNode commentNode;
+            ProtoCore.AST.Node resultNode;
+            resultNode = GraphToDSCompiler.GraphUtilities.Parse(code, out commentNode);
+            IdentifierNode iNode = (resultNode as CodeBlockNode).Body[0] as IdentifierNode;
+            Variable var1 = new Variable(iNode);
+            Assert.AreEqual("a", var1.Name);
+            Assert.AreEqual(1, var1.Row);
+            Assert.AreEqual(1, var1.StartColumn);
+            Assert.AreEqual(2, var1.EndColumn);
+            iNode = null;
+            Assert.Catch<ArgumentNullException>(delegate { Variable var2 = new Variable(iNode); });
+        }
+        [Test]
+        public void TestStatement1()
+        {
+            string code = @"a=b+10;";
+            CodeBlockNode commentNode;
+            ProtoCore.AST.Node resultNode;
+            resultNode = GraphToDSCompiler.GraphUtilities.Parse(code, out commentNode);
+            BinaryExpressionNode ben = (resultNode as CodeBlockNode).Body[0] as BinaryExpressionNode;
+            Statement s1 = Statement.CreateInstance((resultNode as CodeBlockNode).Body[0]);
+            Assert.AreEqual(1, s1.StartLine);
+            Assert.AreEqual(1, s1.EndLine);
+            Assert.AreEqual("a", s1.AssignedVariable.Name);
+            Assert.AreEqual("b", s1.ReferencedVariables[0].Name);
+            Assert.AreEqual(1, s1.ReferencedVariables.Count);
+        }
+
+    }
+}
